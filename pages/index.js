028e4964b2a894e0d7b4bfdc83a1667dbb0972c6
Patrick Lavin <plavin997@gmail.com>
2020-04-25 13:09:23 -0400

    add dark mode

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Date from '../components/date'
+import Darkmode from 'darkmode-js'
+
+new Darkmode().showWidget();
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
